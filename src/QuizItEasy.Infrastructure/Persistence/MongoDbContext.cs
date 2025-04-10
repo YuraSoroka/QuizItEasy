e197cf71206256ced1a837e2115eb51e9b628f57
Yurii Soroka <yurasoroka1999@gmail.com>
2025-04-10 20:00:35 +0300

    Changed formatting

--- src/QuizItEasy.Infrastructure/Persistence/MongoDbContext.cs
+++ src/QuizItEasy.Infrastructure/Persistence/MongoDbContext.cs
@@ -7,13 +7,13 @@ public class MongoDbContext : IMongoDbContext
 {
     public IMongoDatabase Database { get; }
     public IMongoClient MongoClient { get; }
-    
+
     public MongoDbContext()
     {
         MongoClient = new MongoClient("connection_string");
         Database = MongoClient.GetDatabase("quiziteasy");
     }
-    
+
     public IMongoCollection<T> GetCollection<T>(string? name = null)
     {
         return Database.GetCollection<T>(name ?? typeof(T).Name.ToLower());
