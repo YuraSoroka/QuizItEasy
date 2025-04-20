1546aa5f341d3e18cf67caf56082ea8e09e8929d
Yurii Soroka <yurasoroka1999@gmail.com>
2025-04-20 19:11:05 +0300

    Configured MongodbContext infrastructure and registration

--- src/QuizItEasy.Infrastructure/ConfigureServices.cs
+++ src/QuizItEasy.Infrastructure/ConfigureServices.cs
@@ -1,9 +1,11 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Options;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using QuizItEasy.Application.Common.Abstractions;
+using QuizItEasy.Domain.Entities.Common;
+using QuizItEasy.Domain.Entities.Questions;
 using QuizItEasy.Infrastructure.Persistence;
 
 namespace QuizItEasy.Infrastructure;
@@ -12,6 +14,17 @@ public static class ConfigureServices
 {
     public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        var mongoConnectionString = configuration.GetConnectionString("Mongo");
+        var mongoClientSettings = MongoClientSettings.FromConnectionString(mongoConnectionString);
+
+        //mongoClientSettings.ClusterConfigurator = c => c.Subscribe(
+        //    new DiagnosticsActivityEventSubscriber(
+        //        new InstrumentationOptions
+        //        {
+        //            CaptureCommandText = true
+        //        }));
+
+        services.AddSingleton<IMongoClient>(new MongoClient(mongoClientSettings));
         services.AddScoped<IMongoDbContext, MongoDbContext>();
 
         services
@@ -21,6 +34,22 @@ public static class ConfigureServices
                 "MongoDB",
                 HealthStatus.Unhealthy,
                 ["DB", "NoSql", "Mongo"],
-                timeout: TimeSpan.FromSeconds(5));
+                TimeSpan.FromSeconds(5));
+
+        RegisterEntitiesMongoConfiguration();
+    }
+
+    private static void RegisterEntitiesMongoConfiguration()
+    {
+        BsonClassMap.RegisterClassMap<Question>(classMap =>
+        {
+            classMap
+                .MapIdProperty(e => e.Id);
+        });
+
+        BsonClassMap.RegisterClassMap<SingleSelect>(classMap =>
+        {
+            classMap.MapMember(e => e.Answers);
+        });
     }
 }
