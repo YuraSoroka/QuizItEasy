74da0be21a8a307f3ef7c8c0534d5ca5445c924b
Yurii Soroka <yurasoroka1999@gmail.com>
2025-04-20 19:11:26 +0300

    Updated create single-select endpoint

--- src/QuizItEasy.API/Endpoints/CreateSingleSelect.cs
+++ src/QuizItEasy.API/Endpoints/CreateSingleSelect.cs
@@ -1,15 +1,27 @@
 using QuizItEasy.API.Common;
+using QuizItEasy.Application.Common.Abstractions;
+using QuizItEasy.Domain.Entities.Common;
+using QuizItEasy.Domain.Entities.Questions;
 
 namespace QuizItEasy.API.Endpoints;
 
-public class CreateSingleSelect : IEndpoint
+public sealed class CreateSingleSelect : IEndpoint
 {
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapPost("single-select", new Func<object>(() =>
+        app.MapPost("single-select", (IMongoDbContext mongoDbContext) =>
             {
+                var collection = mongoDbContext.GetCollection<Question>("AZ-204");
+                
+                IEnumerable<Answer> answers = 
+                [
+                    Answer.CorrectOption("True"), 
+                    Answer.WrongOption("False")
+                ];
+                collection.InsertOne(SingleSelect.Create(answers, "Test question"));
+
                 return Results.Ok();
-            }))
+            })
             .WithTags(Tags.SingleSelect);
     }
 }
