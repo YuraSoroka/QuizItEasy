cb80197e39b84a794a204629fd5b16d3e019b441
Yurii Soroka <yurasoroka1999@gmail.com>
2025-05-17 17:56:56 +0300

    Created question to json resolvers

---
+++ src/QuizItEasy.Application/Common/Services/QuestionToJsonResolverRegistry.cs
@@ -0,0 +1,28 @@
+﻿using QuizItEasy.Application.Common.Abstractions;
+using QuizItEasy.Application.Common.Models;
+using QuizItEasy.Domain.Common.Extensions;
+using QuizItEasy.Domain.Entities.Common;
+
+namespace QuizItEasy.Application.Common.Services;
+
+public class QuestionToJsonResolverRegistry
+{
+    private readonly Dictionary<string, IQuestionToJsonResolver> _questionResolvers;
+
+    public QuestionToJsonResolverRegistry(IEnumerable<IQuestionToJsonResolver> questionResolvers)
+    {
+        _questionResolvers = questionResolvers.ToDictionary(m => m.QuestionDiscriminator);
+    }
+
+    internal QuestionResponse ResolveAsJson(Question question)
+    {
+        var discriminator = DomainExtensions.GetTypeDiscriminator(question);
+
+        if (_questionResolvers.TryGetValue(discriminator, out var resolver))
+        {
+            return resolver.Resolve(question);
+        }
+
+        throw new NotSupportedException($"No mapper registered for question type: {discriminator}");
+    }
+}
